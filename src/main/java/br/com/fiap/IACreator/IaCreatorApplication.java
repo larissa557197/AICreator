2cfc44da9d075f95c8c19fec63008f1a31227092
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 07:58:58 -0300

    add dados no compose

---
+++ src/main/java/br/com/fiap/IACreator/IaCreatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.IACreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/IACreator/IaCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.IACreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
